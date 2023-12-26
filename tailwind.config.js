1a208e0fad99cccb0b5e5bef1628e5ee30d77833
hellomraza <hellomraza@gmail.com>
2023-12-26 20:43:49 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2A4BA0',
+        secondary: '#1E222B',
+        tertiary: '#616A7D',
+        accent: '#ffc800',
+      },
+    },
   },
   plugins: [],
 };
